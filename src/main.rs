953dcc1b290f3a457904614e6481f6a42fd4ca85
nooooooooob <ike_233@qq.com>
2025-07-31 23:44:02 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use minigrep::Config;
+use std::{env, process};
+
+/// cargo run -- search-string example-filename.txt
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // dbg!(&args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
+    }
+}
